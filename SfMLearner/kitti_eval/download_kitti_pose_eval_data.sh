2f10af3028aa4fd1c3d751befe7ba9250d1aaba8
vrlab <jiafeng5513@outlook.com>
2019-06-05 10:46:45 +0800

    add rgbd_benchmark_tools,eval in depth and pose are passed

--- SfMLearner/kitti_eval/download_kitti_pose_eval_data.sh
+++ SfMLearner/kitti_eval/download_kitti_pose_eval_data.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/env bash
 URL=http://people.eecs.berkeley.edu/~tinghuiz/projects/SfMLearner/pose_eval_data.tar
 TAR_FILE=./kitti_eval/pose_eval_data.tar
-wget -N $URL -O $TAR_FILE
+wget -N $URL $TAR_FILE
 tar -xvf $TAR_FILE -C ./kitti_eval/
 rm $TAR_FILE
