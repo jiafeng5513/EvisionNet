9ae9529f7a78ba55fb3778c1431062fc7c9c06a5
AsusLaptop <jiafeng5513@outlook.com>
2019-10-20 20:26:54 +0800

    change UI

--- Modle2.0/Helper.py
+++ Modle2.0/Helper.py
@@ -1,19 +1,19 @@
 # -*- coding:UTF-8 -*-
 import sys
-import EvisionNet_GUI  # TODO: Change according to the file name of the main window
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog,QFileDialog,QMessageBox
+import UI.EvisionNet_GUI  # TODO: Change according to the file name of the main window
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 
 
 class EvisionNetGUI(QMainWindow):
     def __init__(self):
         super(EvisionNetGUI, self).__init__()
-        self.ui = EvisionNet_GUI.Ui_MainWindow()
+        self.ui = UI.EvisionNet_GUI.Ui_MainWindow()
         self.ui.setupUi(self)
         # signal and solts
         self.signals_handler()
 
     def signals_handler(self):
-        self.ui.pushButton_brewse.clicked.connect(self.OnPushed_broswer)
+        self.ui.pushButton_browse.clicked.connect(self.OnPushed_broswer)
         pass
 
     def OnPushed_broswer(self):
@@ -25,7 +25,7 @@ class EvisionNetGUI(QMainWindow):
                 f = open(fname[0], 'r')
                 with f:
                     data = f.read()
-                    self.textEdit.setText(data)
+                    self.ui.lineEdit.setText(data)
             except:
                 self.textEdit.setText("NOOOOO!")
         pass
